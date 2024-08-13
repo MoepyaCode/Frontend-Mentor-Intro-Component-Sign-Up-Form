68e0619df617fed2bce7ea8e2427636f4e320dc4
MoepyaCode <kagisomoepya19@gmail.com>
2024-08-14 00:04:22 +0200

    🎉 Project init intro-component-with-sign-up-form

---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+import bgIntroDesktop from './bg-intro-desktop.png';
+import bgIntroMobile from './bg-intro-mobile.png';
+import iconError from './icon-error.svg';
+
+export const Assets = {
+    bgIntroDesktop,
+    bgIntroMobile,
+    iconError
+}
